20eb80133ddcea4bba3df8bcefa1aa35188b87fe
Mattvil04 <matisebastianvillacis@gmail.com>
2024-03-08 16:31:34 -0500

    updated

--- scripts/getDates.js
+++
@@ -1,5 +0,0 @@
-// JavaScript to set current year and last modified date
-document.getElementById('year').innerText = new Date().getFullYear();
-
-var lastModified = new Date(document.lastModified);
-document.getElementById('lastModified').innerText = 'Last modified: ' + lastModified.toLocaleString();
