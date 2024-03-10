005c5f4e9227388de646d544efdef8ec778df23e
Mattvil04 <matisebastianvillacis@gmail.com>
2024-03-10 13:11:24 -0500

    updated

--- Week1/scripts/getDates.js
+++ Week1/scripts/getDates.js
@@ -3,3 +3,10 @@ document.getElementById('year').innerText = new Date().getFullYear();
 
 var lastModified = new Date(document.lastModified);
 document.getElementById('lastModified').innerText = 'Last modified: ' + lastModified.toLocaleString();
+
+const hamburgerElement = document.querySelector('#menuButton');
+const navElement = document.querySelector('.menuLinks')
+hamburgerElement.addEventListener("click", () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
