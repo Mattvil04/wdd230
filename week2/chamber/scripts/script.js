d3440ef1953f24435efb66bd806aae008827ad96
Mattvil04 <matisebastianvillacis@gmail.com>
2024-03-21 11:43:51 -0500

    updated chamber

--- week2/chamber/scripts/script.js
+++ week2/chamber/scripts/script.js
@@ -1,1 +1,17 @@
+// JavaScript to set current year and last modified date
+document.getElementById('year').innerText = new Date().getFullYear();
 
+var lastModified = new Date(document.lastModified);
+document.getElementById('lastModified').innerText = 'Last modified: ' + lastModified.toLocaleString();
+
+const hamburgerElement = document.querySelector('#menuButton');
+const navElement = document.querySelector('.menuLinks')
+hamburgerElement.addEventListener("click", () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
+const darkmodeElement = document.querySelector('#darkMode')
+const mainElement =document.querySelector("main")
+darkmodeElement.addEventListener('click', () => {
+    mainElement.classList.toggle('open');
+})
